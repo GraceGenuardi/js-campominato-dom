746d6bcd57baa5d6e0d5844c5821b8db2529fdfa
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-10 15:26:07 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+console.log('Prato fiorito')
+
+//PRIMA PARTE: CREAZIONE GRIGLIA
+
+//1. Dichiariamo le costanti 
+const latoGriglia = 10
+const numeroCelle = latoGriglia * latoGriglia
+
+console.log(latoGriglia, numeroCelle)
+
+const grigliaElement = document.querySelector('.griglia')
+console.log(grigliaElement)
+
+const playButton = document.querySelector('#play-button')
+
+//2.L’utente clicca su un pulsante PLAY  per generare la una griglia quadrata 10 * 10 
+playButton.addEventListener('click', function () { //NB Correggi dando nome alla funzione
+
+	 // Azzera la griglia
+	 grigliaElement.innerHTML = '';
+	 
+	for (let i = 0; i < numeroCelle; i++) {
+		let num = i + 1
+		console.log(num)
+		let divString = `<div class="cella" style="width: calc(100% / ${latoGriglia});" >${num}</div>` 
+	
+		grigliaElement.innerHTML += divString
+	}
+	
+	const celleElements = document.querySelectorAll('.cella')
+	// console.log(celleElements)
+
+//3.Ogni casella deve contenere il numero corrispondente a partire da 1 fino a 100
+
+	for (let i = 0; i < celleElements.length; i++) {
+		const cella = celleElements[i]
+	
+		cella.addEventListener('click', function () {
+			console.log(i + 1)
+		})
+
+ //4. BONUS: cambio background delle celle cal click
+ 
+ cella.addEventListener('click', function () {
+    console.log(i + 1)
+    cella.classList.toggle("cella-selezionata")
+})
+   
+
+
+
+	}
+})
+
+function onClick() {
+	console.log(i + 1)
+}
+
+
+//SECONDA PARTE: GENERAZIONE BOMBE
+
+//1. Creare constante per il numero di bombe da inserire e un array vuoto per tenerne traccia
+
+
+//2. Generare 16 numeri casuali unici che rappresentano le posizioni delle bombe
+
+//3. Modificare l'event listener per il click sulla cella
+
+//4. Partita conclusa, comunico punteggio
